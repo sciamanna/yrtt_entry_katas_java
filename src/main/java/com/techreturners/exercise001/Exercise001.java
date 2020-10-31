ae3d7640fb8c54f719898c616c50aeb4a48d33f6
Rebeca <rebeca@henchman-29.sourceofevil.org>
2020-10-31 12:15:13 +0000

    Finished exercise 1

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -6,12 +6,42 @@ public class Exercise001 {
     // Your task will be to return the sum of the numbers that occur only once.
     // For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15.
 
-    // More examples in exercise001 test cases.
-
-    // Good luck!
+    public static void main(String[] args) {
+        Exercise001 ex1 = new Exercise001();
+        int result = ex1.singles(new int []{4, 5, 7, 5, 4, 8});
+        System.out.println("Result is " + result);
+    }
 
     public int singles(int [] arr){
-        // Add your code here!
-        return 0;
+        // The function takes an array as the parameter
+        // Loop through and compare item with all others
+        int sum = 0;
+        boolean unique;
+
+        for (int i=0; i<arr.length; i++)
+        {
+            System.out.println("Starting position: " + i + " value: " + arr[i]);
+            int item=arr[i];
+            unique = true;
+
+            for (int y=0; y<arr.length; y++)
+            {
+                if (i==y)
+                    continue;
+
+                System.out.println("Comparing position: " + y + " value: " + arr[y] + " to value: " + item);
+                if (item == arr[y]){
+                    unique = false;
+                    System.out.println("Value: " + item + " is not unique, skipping");
+                    break;
+                }
+            }    
+            if (unique == true) {
+                System.out.println("Value: " + item + " is unique, adding to total");
+                sum = sum + item;  
+                System.out.println("Sum is now: " + sum);
+            }
+        }
+        return sum;
     }
 }
